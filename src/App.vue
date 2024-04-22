b1cadcaa48f0d25535c7c310cf1247ed568f4705
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-21 20:18:06 -0400

    nuked previous attempt, this marks the start of a new one

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Task Manager:</h1>
+    <button> Add a Task</button>
+
+
+
+  </div>
+</template>
+
+
+
+<script setup>
+
+
+
+
+</script>
+
+
+
+
+
+
+
+<style>
+
+</style>
