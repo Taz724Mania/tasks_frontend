51d98490083b2254ce4654f7366c93eafac5705e
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-21 21:08:26 -0400

    buttons added

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     <h2>{{ task.details }}</h2>
     <h2>{{ task.dueDateTime }}</h2>
     <h2>{{ task.completed }}</h2>
-
-
+    <button>Edit this Task</button>
+    <button>Delete this Task</button>
 
   </div>
 </template>
@@ -43,12 +43,42 @@ const addTask = async (newTask) => {
     tasks.value.push(data);
     showForm.value = false;
   } catch (error) {
-    console.error('Error adding task:', error);
+    console.error('Could not add task:', error);
+  }
+};
+
+const editTask = async (task) => {
+  try {
+    const response = await fetch(`${url}${task.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task),
+    });
+
+    if (response.ok) {
+      console.log('Task updated successfully!');
+    } else {
+      console.error('Error updating task:', await response.text());
+    }
+  } catch (error) {
+    console.error('Error saving task:', error);
   }
 };
 
 
+const deleteTask = async (taskId) => {
+  try {
+    const response = await fetch(`${url}${taskId}`, { method: 'DELETE' });
 
+    if (response.ok) {
+      tasks.value = tasks.value.filter((task) => task.id !== taskId);
+    } else {
+      console.error('Could not Delete task', await response.text());
+    }
+  } catch (error) {
+    console.error('Could not Delete task', error);
+  }
+};
 
 
 onMounted(getTasks)
