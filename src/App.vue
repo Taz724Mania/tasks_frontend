ff757db5cde4a4f21ef3680dad65d9f979b8720a
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-21 20:31:29 -0400

    can get tasks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>Task Manager:</h1>
+    <h1>Task Manager :</h1>
     <button> Add a Task</button>
+    <h2>{{ task.title }}</h2>
+    <h2>{{ task.details }}</h2>
+    <h2>{{ task.dueDateTime }}</h2>
+    <h2>{{ task.completed }}</h2>
 
 
 
@@ -11,14 +15,28 @@
 
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const url = 'https://tasks-backend-bws4.onrender.com/task/'
+const tasks = ref([])
+
+const getTasks = async () => {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    tasks.value = data
+  } catch (error) {
+    console.log("Tasks not Found", error)
+  }
+}
 
 
 
 
+
+
+onMounted(getTasks)
 </script>
-
-
-
 
 
 
